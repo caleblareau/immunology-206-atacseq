386ef592ce8f03c984028c94b5d095722ed8f464
caleblareau <clareau@stanford.edu>
2022-02-24 12:27:51 -0800

    two analysis scripts

--- code/01_eda.R
+++ code/01_eda.R
@@ -1,2 +1,24 @@
-library(DESeq2)
+# Installations
+if(FALSE){
+  
+  # Install package for general analysis
+  install.packages("pheatmap")
+  install.packages("data.table")
+  install.packages("dplyr")
+  
+}
 
+library(dplyr)
+library(data.table)
+library(pheatmap)
+
+tab <- fread("../data/GSE74912_ATACseq_All_Counts.txt.gz")
+meta <- fread("../data/samplelookup.tsv")[1:80,]
+
+# Some ugly code just to subset to the samples that we have in the meta data
+counts_mat <- data.matrix(data.frame(tab[,meta$SampleName, with = FALSE]) )
+
+# Make the names pretty
+colnames(counts_mat) <- make.unique(meta$CellType)
+
+pheatmap(cor(counts_mat))
--- code/02_deseq.R
+++ code/02_deseq.R
@@ -20,16 +20,26 @@ library(GenomicRanges)
 library(dplyr)
 library(data.table)
 
+#grch37 is loaded from the annotables package
+
 # Build a simplified data frame of gene coordinates
 data.frame(
   chr = paste0("chr", grch37$chr), 
   start = grch37$start,
   end = grch37$end,
   gene = grch37$symbol
-) %>% makeGRangesFromDataFrame() -> gene_coordinates
+) %>% makeGRangesFromDataFrame(keep.extra.columns = TRUE) -> gene_coordinates
 
 # Import the ATAC-seq data
 tab <- fread("../data/GSE74912_ATACseq_All_Counts.txt.gz")
+makeGRangesFromDataFrame(
+  data.frame(tab[,c(1:3)])
+) -> atac_peak_coordinates
+index_of_nearest_gene <- nearest(atac_peak_coordinates, gene_coordinates)
+atac_peak_coordinates$gene <- gene_coordinates$gene[index_of_nearest_gene]
+head(atac_peak_coordinates)
+
+# now import meta data
 meta <- fread("../data/samplelookup.tsv") %>%
   filter(CellType %in% c("CD8Tcell", "NKcell"))
 
@@ -49,5 +59,15 @@ res <- lfcShrink(dds, coef="CellType_NKcell_vs_CD8Tcell", type="apeglm")
 
 # Now put all of the results into a dataframe
 data.frame(
+  atac_peak_coordinates,
   res
-)
+) -> full_atac_deseq2_peak_results
+full_atac_deseq2_peak_results$string <- paste0(full_atac_deseq2_peak_results$seqnames, ":",
+                                               full_atac_deseq2_peak_results$start, "-",
+                                               full_atac_deseq2_peak_results$end)
+# Look at top hits
+top_hits <- full_atac_deseq2_peak_results[!is.na(full_atac_deseq2_peak_results$padj),] %>%
+  arrange(pvalue)
+
+head(top_hits)
+
